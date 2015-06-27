08d03449acc27d25bd82f22346ff7c49e34c2a65
Nicole Ahima <Nicole.Ahima@outlook.com>
2015-06-27 12:05:45 -0500

    Items in Calendar

--- JS/MainCtrl.js
+++ JS/MainCtrl.js
@@ -1,23 +1,70 @@
-
 app.controller("MainCtrl", ["$scope", "$firebaseArray", "$http",
-  function($scope, $firebaseArray, $http) {
-    var homesRef = new Firebase("https://residential.firebaseio.com/homes");
+    function($scope, $firebaseArray, $http) {
+        var homesRef = new Firebase("https://residential.firebaseio.com/homes");
 
-    // download the data from a Firebase database reference into a (pseudo read-only) array
-    // all server changes are applied in realtime
-    $scope.homes = $firebaseArray(homesRef);
+        // download the data from a Firebase database reference into a (pseudo read-only) array
+        // all server changes are applied in realtime
+        $scope.homes = $firebaseArray(homesRef);
+        console.log('$scope.homes',$scope.homes)
 
-    // create a query for the most recent 25 messages on the server
-    var query = homesRef.orderByChild("timestamp").limitToLast(25);
+        // create a query for the most recent 25 messages on the server
+        // var query = homesRef
+        var homesArray = [ ]
 
-    // the $firebaseArray service properly handles Firebase database queries as well
-    $scope.filteredHomes = $firebaseArray(query);
+        // the $firebaseArray service properly handles Firebase database queries as well
+        // $scope.filteredHomes = $firebaseArray(homesRef);
+          $scope.homes.$loaded()
+            .then(function(){
+                angular.forEach($scope.homes, function(currentHome) {
+                    var eventObject = {
+                        title: currentHome.Site,
+                        start: currentHome.Date,
+                        allDay: true
+                    }
+                    homesArray.push(eventObject)
+                });
+                console.log('homesArray',homesArray)
+                $('#calendar').fullCalendar({
+                    events: homesArray
+                });
+            });
 
-    $('#calendar').fullCalendar({
-         defaultView: 'agendaWeek'
-    })
 
-  }
+
+        // $('#calendar').fullCalendar({
+        //     homesArray: [],
+        //     defaultView: 'agendaWeek',
+
+        //     select: function(start, end, allDay) {
+        //         /*
+        //             after selection user will be promted for enter title for event.
+        //         */
+        //         var  title = prompt('Event Title:');
+        //         /*
+        //             if title is enterd calendar will add title and event into fullCalendar.
+        //         */
+        //         if (homes) {
+        //             calendar.fullCalendar('renderEvent', {
+        //                     title: Site,
+        //                     start: start,
+        //                     end: end,
+        //                     allDay: allDay
+        //                 },
+        //                 true // make the event "stick"
+        //             );
+        //         }
+        //         calendar.fullCalendar();
+        //     },
+        //     /*
+        //         editable: true allow user to edit events.
+        //     */
+        //     editable: false,
+        //     /*
+        //         events is the main option for calendar.
+        //         for demo we have added predefined events in json object.
+        //     */
+
+        // });
+
+    }
 ]);
-
-
--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,4 @@ app.config(function ($routeProvider) {
 
 });
 
+
